b8869a88e0b9d48e451f687391753a9df90439cd
sea-shunned <cameron.shand@manchester.ac.uk>
2018-12-02 19:24:24 +0000

    added day 2

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ def load_data(day_num, part_num=None):
 	if part_num is None:
 		fname = f"day{day_num}_input.txt"
 	else:
-		fname = f"day{day_num}_input_part{part_num}.txt"
+		fname = f"day{day_num}_input_p{part_num}.txt"
 	if os.path.isfile(fname):
 		return fname
 	else:
