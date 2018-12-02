a69260fe847b9ffdb26047f572d910429ebce171
sea-shunned <cameron.shand@manchester.ac.uk>
2018-12-02 18:26:30 +0000

    Initial commit with day1

---
+++ day1_code.py
@@ -0,0 +1,8 @@
+import utils
+
+fname = utils.load_data(1)
+with open(fname,"r") as file:
+	vals = file.read().splitlines()
+	vals = [int(i) for i in vals]
+
+print(sum(vals))
---
+++ day1_code_p2.py
@@ -0,0 +1,19 @@
+import utils
+from itertools import cycle
+
+fname = utils.load_data(1)
+file = open(fname,"r")
+vals = [int(i) for i in file.read().splitlines()]
+file.close()
+
+freq = 0
+seen_freqs = set()
+seen_freqs.add(freq)
+
+for val in cycle(vals):
+	freq += val
+	if freq in seen_freqs:
+		print(freq)
+		break
+	else:
+		seen_freqs.add(freq)
