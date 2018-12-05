01411b7f1bed03253a5b63e3b3d846e8ce824323
sea-shunned <cameron.shand@manchester.ac.uk>
2018-12-05 12:57:55 +0000

    Added code/comment to show 5x speed-up

--- day05/day05_p2.py
+++ day05/day05_p2.py
@@ -3,6 +3,11 @@ import utils
 with open("input.txt", "r") as file:
     # Strip the newline off
     raw_data = file.read().strip()
+
+## We can apply part 1 first to shorten the input
+# Roughly 5x speed-up
+# raw_data = utils.react_polymers(raw_data)
+
 # Only look at the letters that exist
 uniq_letters = set([i.lower() for i in raw_data])
 # Dict to store results
